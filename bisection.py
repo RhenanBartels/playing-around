be2b5600822f0f7819df2a13f19d94e6005069ed
Rhenan Bartels <rhenan.bartels@gmail.com>
2013-06-09 01:56:48 -0300

    Bisection Method

---
+++ bisection.py
@@ -0,0 +1,19 @@
+# _*_ coding: utf-8
+
+
+def bisMethod(fx, xl, xr, Epsilon=10e-4):
+
+    fl = fx(xl)
+    fr = fx(xr)
+    if fl * fr > 0:
+        raise Exception('xl and xr are not bracketing the solution')
+    else:
+        nx = (xl + xr) / 2.
+        while abs(nx - xl) > Epsilon:
+            nf = fx(nx)
+            if fl * nf < 0:
+                xr = nx
+            else:
+                xl = nx
+            nx = (xl + xr) / 2
+    return nx
